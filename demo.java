f4d86c5f42440c3740d10bc36af4652b993e7f97
royalriderr007@gmail.com <meghana242005@gmail.com>
2025-04-24 11:07:46 +0530

    save

--- demo.java
+++ demo.java
@@ -3,9 +3,9 @@ class demo{
         int a=10;
         int b=15;
         int x,y;
-        x=a+b;
-        y=a-b;
-        System.out.println("Addition is : " +x);
-        System.out.println("Substraction is :"+y);
+        x=a/b;
+        y=a*b;
+        System.out.println("divison is : " +x);
+        System.out.println("Multiplication is :"+y);
     }
 }
